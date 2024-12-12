291446465fc8692747b7550d627a1ffbebf60d96
ISXI15 <110128759+ISXI15@users.noreply.github.com>
2024-12-12 10:35:50 +0100

    com

--- src/app/finanzplaner/page.tsx
+++ src/app/finanzplaner/page.tsx
@@ -3,15 +3,14 @@ import { redirect } from 'next/navigation'
 import FinanzplanerContent from './FinanzplanerContent'
 
 export default function FinanzplanerPage() {
-  // Get the cookie store
   const cookieStore = cookies()
   const token = cookieStore.get('token')
 
-  // If there's no token, redirect to login
+  // Redirect to Login Page
   if (!token) {
     redirect('/login')
   }
 
-  // If we have a token, render the finanzplaner content
+  // Redirect to FPContent
   return <FinanzplanerContent />
 }
